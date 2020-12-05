be4add8e0fef53947452c28cef4ae7c4402f029a
Oscar <oscar.a.chavez.jr@gmail.com>
2020-12-04 20:38:38 -0700

    debugging even more

--- index.js
+++ index.js
@@ -35,5 +35,5 @@ app.use('/lifts', liftsRouter);
 var PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-  console.log('Server is running on port 3001...');
+  console.log(`Server is running on port ${PORT}...`);
 })
--- routes/users.js
+++ routes/users.js
@@ -6,51 +6,56 @@ const auth = require('../middleware/auth');
 
 // MAKE MAIN LOG IN ROUTE
 router.route('/signin').post(async (req, res) => {
-  const {username, password} = req.body;
-  const user = await User.findOne({username: username});
+  try{
+    const {username, password} = req.body;
+      const user = await User.findOne({username: username});
 
-  if(!user){
-    // create account
-        // password hashing
-        const salt = await bcrypt.genSalt();
-        const passwordHash = await bcrypt.hash(password, salt);
-        // create new mongo User
-        const newUser = new User({username: username, password: passwordHash});
+      if(!user){
+        // create account
+            // password hashing
+            const salt = await bcrypt.genSalt();
+            const passwordHash = await bcrypt.hash(password, salt);
+            // create new mongo User
+            const newUser = new User({username: username, password: passwordHash});
 
-        await newUser.save()
-          .then(async () => {
-            const newlySignInUser = await User.findOne({username: username});
-            // get token for newly created user
-            const token = jwt.sign({id: newlySignInUser._id}, process.env.JWT_SECRET)
-            res.json({
-                        token,
-                        user: {
-                          id: newlySignInUser._id,
-                          username: newlySignInUser.username
-                        }
-                    }) 
-          })
-          .catch(err => res.status(400).json('Error: ' + err));
+            await newUser.save()
+              .then(async () => {
+                const newlySignInUser = await User.findOne({username: username});
+                // get token for newly created user
+                const token = jwt.sign({id: newlySignInUser._id}, process.env.JWT_SECRET)
+                res.json({
+                            token,
+                            user: {
+                              id: newlySignInUser._id,
+                              username: newlySignInUser.username
+                            }
+                        }) 
+              })
+              .catch(err => res.status(400).json('Error: ' + err));
 
-          
-           
-  } else {
-      // if account with username exists, check password
-    const isMatch = await bcrypt.compare(password, user.password);
-    // if passwords match, sign in and create web token
-      if(!isMatch){
-        return res.status(400).json({ msg: "Invalid credentials... Password and email on file do not match..."})
+              
+              
       } else {
-        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET)
-          res.json({
-            token,
-            user: {
-              id: user._id,
-              username: user.username
-            }
-        })  
-      }     
+          // if account with username exists, check password
+        const isMatch = await bcrypt.compare(password, user.password);
+        // if passwords match, sign in and create web token
+          if(!isMatch){
+            return res.status(400).json({ msg: "Invalid credentials... Password and email on file do not match..."})
+          } else {
+            const token = jwt.sign({id: user._id}, process.env.JWT_SECRET)
+              res.json({
+                token,
+                user: {
+                  id: user._id,
+                  username: user.username
+                }
+            })  
+          }     
+      }
+  } catch (error) {
+    res.status(500).json({error: error.message})
   }
+  
 });
 
 router.route("/tokenIsValid").post(async (req, res) => {
